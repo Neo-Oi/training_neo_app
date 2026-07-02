be491be201f22ca6fc2a6835fca29c7831819a0a
Neo-Oi <neo-oi@comthink.co.jp>
2026-07-02 17:05:39 +0900

    ユーザー作成途中段階。作成確認できず、validatesが途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,28 @@ def user_role_init
     flash[:notice] = "管理者です"
     end
 end
+
+def show
+    @user = User.find_by(params[:id])
+end
+
+def new
+    @user = User.new
+end
+
+def create
+    @user = User.new(user_params)
+    if @user.save
+        redirect_to users_path
+    else
+        render 'new'
+    end
+end
+
+private
+
+def user_params
+    params.require(:user).permit(:name,:unconfirmed_email)
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   enum :role, {member: 0, admin: 1}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :name,length:{maximum:255}
+  validates :email,uniqueness: true,format: { with: Devise.email_regexp }
+  validates :password,length:{maximum:255},length:{minimum:6}
+
+  
   devise :database_authenticatable, :rememberable, :validatable
 end

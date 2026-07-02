f2399b1fb944f63f7ff28b3e6470ea210c067214
Neo-Oi <neo-oi@comthink.co.jp>
2026-07-02 11:16:25 +0900

    権限区分実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
 before_action :user_init
+before_action :user_role_init, only: [:new, :edit, :index]
 
 def user_init
    redirect_to new_user_session_path if user_signed_in? == false
 end
 
 def user_render
-    render :"ログイン中:#{user.name}"
+    render :"ログイン中:#{current_user.name}"
 end
 
+def user_role_init
+    if current_user.member?        
+        flash[:notice] = "権限がありません"
+        redirect_to user_path(current_user)
+    else
+    flash[:notice] = "管理者です"
+    end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum :role, {member: 0, admin: 1}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
